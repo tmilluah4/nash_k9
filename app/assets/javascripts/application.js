c88f4789892e743a840cc2ca553d97f954f0f737
tmilluah4 <todd_miller@onlifehealth.com>
2012-11-02 15:52:12 -0500

    sdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,26 +28,9 @@ $(document).ready(function() {
     });
 });
 
-$('.box').each(function() {
-    $(this).before($('<span>').text("■"));
-});
 
 
 
 
-
-
-function setActive()
-{
-    aObj2 =   document.getElementById('navSidebar').getElementsByTagName('a');
-    for(i=0;i<aObj2.length;i++)
-    {
-        if(document.location.href.indexOf(aObj2[i].href)>=0)
-        {
-            aObj2[i].className='active';
-        }
-    }
-}
-window.onload = setActive;
 
 
