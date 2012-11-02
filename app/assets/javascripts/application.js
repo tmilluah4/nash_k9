d51965f430025b1e8a93df5cfb4f57ff26298ea8
tmilluah4 <todd_miller@onlifehealth.com>
2012-11-02 15:56:37 -0500

    sdfd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,39 @@ $(document).ready(function() {
 });
 
 
+function setActive()
+{
+    aObj2 =   document.getElementById('navSidebar').getElementsByTagName('a');
+    for(i=0;i<aObj2.length;i++)
+    {
+        if(document.location.href.indexOf(aObj2[i].href)>=0)
+        {
+            aObj2[i].className='active';
+        }
+    }
+}
+window.onload = setActive;
+
+
+
+
+var theImages = new Array()
+
+theImages[0] = '/assets/dog_training_overview/dog-training-overview-1.jpg'
+theImages[1] = '/assets/dog_training_overview/dog-training-overview-2.jpg'
+
+var j = 0
+var p = theImages.length;
+var preBuffer = new Array()
+for (i = 0; i < p; i++){
+    preBuffer[i] = new Image()
+    preBuffer[i].src = theImages[i]
+}
+var whichImage = Math.round(Math.random()*(p-1));
+function showImage(){
+    document.write('<img src="'+theImages[whichImage]+'">');
+}
+
 
 
 
