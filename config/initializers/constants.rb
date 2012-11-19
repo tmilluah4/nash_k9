b0962eebfc07a8ba7ac29d382e0ba76001987eb0
tmilluah4 <todd_miller3@onlifehealth.com>
2012-11-19 08:35:25 -0600

    prod

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 APP_ID= '384540454954612' # please change!
 APP_SECRET= '5cad8eb3f2b7211c13574a6a31919b80' # please change!
-SITE_URL = 'http://localhost:3000'
+SITE_URL = 'http://enigmatic-lowlands-3602.herokuapp.com/'
