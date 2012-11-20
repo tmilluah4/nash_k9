3adfad661eb46e309cf70d71fbe7518df67565d6
tmilluah4 <todd_miller3@onlifehealth.com>
2012-11-19 18:15:33 -0600

    m

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,6 @@ NashK9::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
   # Change mail delvery to either :smtp, :sendmail, :file, :test
