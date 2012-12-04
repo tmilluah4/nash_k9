752b23260580e34c4de340a054aaf633b3726e57
tmilluah4 <todd_miller@onlifehealth.com>
2012-12-04 06:28:38 -0600

    gallery changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,14 @@ class PagesController < ApplicationController
     session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL)
     @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
     puts session.to_s + "<<< session"
-
     respond_to do |format|
       format.html {  }
     end
-
     if params[:code]
       # acknowledge code and get access token from FB
       session[:access_token] = session[:oauth].get_access_token(params[:code])
     end
-
     # auth established, now do a graph call:
-
      @api = Koala::Facebook::API.new("AAAFdvM04JnQBAMZBmVuYMWes5rrYBmqWXOmhkPjQpenbbZA2qC3Yqq2acC0brNxru91nqj6ZCvdle9ZCeGaTo9Ndd5VwvlvmI0Uj79CkYwZDZD")
     begin
       @stream  = @graph_data = @api.get_object("/me/statuses", "fields"=>"message")
@@ -25,15 +21,16 @@ class PagesController < ApplicationController
     rescue Exception=>ex
       puts ex.message
     end
-
     respond_to do |format|
       format.html {   }
     end
+  end
+
+  def training_videos
 
   end
 
   def dogs
-
     session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/dogs')
     @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
     puts session.to_s + "<<< session"
@@ -46,8 +43,6 @@ class PagesController < ApplicationController
       # acknowledge code and get access token from FB
       session[:access_token] = session[:oauth].get_access_token(params[:code])
     end
-
-    # auth established, now do a graph call:
 
     @api = Koala::Facebook::API.new("AAAFdvM04JnQBAMZBmVuYMWes5rrYBmqWXOmhkPjQpenbbZA2qC3Yqq2acC0brNxru91nqj6ZCvdle9ZCeGaTo9Ndd5VwvlvmI0Uj79CkYwZDZD")
     @aid =  params[:id]
@@ -76,18 +71,6 @@ class PagesController < ApplicationController
           "query1" => "select aid, cover_pid, photo_count, name from album where owner = me() and photo_count > 0  order by created desc",
           "query2" => "select src_big from photo where pid in (select cover_pid from #query1)   "}.to_json})
 
-
-       #@aska
-
-
-
-      #kilo
-
-
-      #@leo
-
-
-
     rescue Exception=>ex
       puts ex.message
     end
@@ -95,13 +78,9 @@ class PagesController < ApplicationController
     respond_to do |format|
       format.html {   }
     end
-
-
   end
 
-  def training_videos
 
-  end
 
   def contact
 
@@ -111,24 +90,17 @@ class PagesController < ApplicationController
     session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/gallery')
     @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
     puts session.to_s + "<<< session"
-
     respond_to do |format|
       format.html {  }
     end
-
     if params[:code]
       # acknowledge code and get access token from FB
       session[:access_token] = session[:oauth].get_access_token(params[:code])
     end
-
-    # auth established, now do a graph call:
-
     @api = Koala::Facebook::API.new("AAAFdvM04JnQBAMZBmVuYMWes5rrYBmqWXOmhkPjQpenbbZA2qC3Yqq2acC0brNxru91nqj6ZCvdle9ZCeGaTo9Ndd5VwvlvmI0Uj79CkYwZDZD")
     @aid =  params[:id]
     begin
-
       @album1  = @api.fql_query("SELECT src_big,src FROM photo WHERE aid = '#{@aid}'" )
-
 
     rescue Exception=>ex
       puts ex.message
@@ -137,7 +109,6 @@ class PagesController < ApplicationController
     respond_to do |format|
       format.html {   }
     end
-
   end
 
   def gallery
@@ -171,49 +142,7 @@ class PagesController < ApplicationController
       format.html {   }
     end
 
-
   end
 
 end
 
-
-def index
-  session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
-  @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
-  puts session.to_s + "<<< session"
-
-  respond_to do |format|
-    format.html {  }
-  end
-end
-
-def callback
-  session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
-  @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream")
-  puts session.to_s + "<<< session"
-
-  respond_to do |format|
-    format.html {  }
-  end
-
-  if params[:code]
-    # acknowledge code and get access token from FB
-    session[:access_token] = session[:oauth].get_access_token(params[:code])
-  end
-
-  # auth established, now do a graph call:
-
-  @api = Koala::Facebook::API.new(session[:access_token])
-  begin
-    @album_graph  = @api.fql_query("SELECT  src_big,src FROM photo WHERE aid = '100000256514752_123407'" )
-
-  rescue Exception=>ex
-    puts ex.message
-  end
-
-  respond_to do |format|
-    format.html {   }
-  end
-
-
-end
