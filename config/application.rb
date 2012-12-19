a080945b8883480e0f696a22035a295c3e00493d
tmilluah4 <todd_miller@onlifehealth.com>
2012-12-19 07:18:05 -0600

    analy

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-if Rails.env == "production"
-  config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-9281447-1")
-end
+#if Rails.env == "production"
+#  config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-9281447-1")
+#end
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
