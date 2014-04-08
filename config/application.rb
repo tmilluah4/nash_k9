3c894cadce89cebdfc652808baf9bbf8b57c55c1
tmilluah4 <todd_miller@onlifehealth.com>
2014-04-08 08:20:27 -0500

    api and partners

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module NashK9
     # paste it at end of line below and go the url...it will generate the new one to use....replace the old api keys with this new key
     # https://graph.facebook.com/oauth/access_token?%20client_id=384540454954612&%20client_secret=5cad8eb3f2b7211c13574a6a31919b80&%20grant_type=fb_exchange_token&%20fb_exchange_token=
 
-  $api = Koala::Facebook::API.new("CAAFdvM04JnQBALUQanh01BB3ix1wDVB11EhfyCMJRe6A3MRyqgZBMZC0U3IY2OGHwTOQvZAAnDCdZATXLsuUmF6ApqDA9iNLZAKNaWiwN2CcZAZB0aE5d4CA151Dbz6WN3JC9gApuyMmImQLuZA9kL4yiPFtwjaWqjm9jR4G7s5LE5ZAzdlsXJ8QPtcXu4T5zMTgZD")
+  $api = Koala::Facebook::API.new("CAAFdvM04JnQBAMUTnIzgh8oaLzZCdyEoQ220yYKkTr1hTDg4RxJjXnr2dQ7aRZA3vhsRGlFMsBbbh8DgZCY6vLYwy22ffbQxpRHEZAUssR2chxviPnlXX6zHtahT1omGjA2x4gx35KVYdZC9HZBcmNAgIpQu4S2SPDuGTi3sQZAGZBUfVZCcBSZB4KlZCH2gWpoy08ZD")
 
 
     # Settings in config/environments/* take precedence over those specified here.
