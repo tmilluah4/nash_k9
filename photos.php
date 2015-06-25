1b15a7007d14a325ded595cb785707f4b48cd912
tmilluah4 <todd_miller@onlifehealth.com>
2015-06-25 18:54:01 -0500

    facebook token

--- photos.php
+++ photos.php
@@ -27,16 +27,17 @@
 
 
 
-				<?php 
-				$access_token = 'access_token=1553215644935364|xvz9dUPZ7dX6Ii0CAMAIMQQTcqE'
+				<?php  
 				 
-					$json_link = "https://graph.facebook.com/v2.3/$album_id}/photos?fields=source&{$access_token}";
+				 
+					$json_link = "https://graph.facebook.com/v2.3/$album_id}/photos?fields=source";
 					$json = file_get_contents($json_link);
 					$obj = json_decode($json, true, 512, JSON_BIGINT_AS_STRING);
 					$photo_count = count($obj['data']);		
 					
-					for($x=0; $x<$photo_count; $x++){
-						 $source = $obj['data'][$x]['source'];
+					for($x=0; $x<$photo_count; $x++){ 
+						 $source = isset($obj['data'][$x]['source']) ? $obj['data'][$x]['source'] : ""; 
+ 
 						
 						 echo "<div class='imgLiquid'  style='-moz-box-shadow:0px 0px 4px #000; -webkit-box-shadow: 0px 0px 4px #000; box-shadow:0px 0px 4px #000;display: inline-block; margin: 10px;width:206px; height:206px'>"; 
 					            	echo "<a href='{$source}' rel='gallery1'  class='fancybox' data-gallery>";
